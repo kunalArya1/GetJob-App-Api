f68acc98bb51dceb850afd19c54d396e0ecb8b4d
Kunal Kumar Arya <kunalkrraj220@gmail.com>
2024-03-18 12:43:00 +0530

    Custom Error , ApiResponse,ApiError File added

---
+++ utils/ApiError.js
@@ -0,0 +1,17 @@
+class ApiError extends Error {
+  constructor(message, statusCode, stack = "") {
+    {
+      super(message);
+      this.statusCode = statusCode;
+      this.message = message;
+      this.data = null;
+      if (stack) {
+        this.stack = stack;
+      } else {
+        Error.captureStackTrace(this, this.constructor);
+      }
+    }
+  }
+}
+
+export { ApiError };
---
+++ utils/CustomError.js
@@ -0,0 +1,8 @@
+const customError = (err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+
+  res.status(statusCode).json({
+    message: err.message,
+    errName: err.name,
+  });
+};
