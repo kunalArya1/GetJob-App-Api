c7b3ba56382375b10233fef741cf8ce1a0ecf065
Kunal Kumar Arya <kunalkrraj220@gmail.com>
2024-03-19 14:27:32 +0530

    Refactor route to use router.route() method

--- routes/Resume.routes.js
+++ routes/Resume.routes.js
@@ -2,5 +2,5 @@ import express from "express";
 import { homePage } from "../controllers/Resume.controllers.js";
 const router = express.Router();
 
-router.get("/", homePage);
+router.route("/").get(homePage);
 export default router;
